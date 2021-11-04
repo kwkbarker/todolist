b05ccd4f55fd14bbfd3db7b1e671ed96fbf1550a
Kevin Barker <kwkbarker@gmail.com>
2021-11-04 16:46:45 -0400

    cloud sql config

--- todolist/__init__.py
+++ todolist/__init__.py
@@ -3,6 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 # from flask_session import Session
 from flask_admin import Admin
+import os
+import pymysql
 
 # initialize app, db, session, hash function, login, admin
 app = Flask(__name__, static_url_path='/todolist/static')
@@ -24,3 +26,29 @@ admin = Admin(app, template_mode='bootstrap3')
 
 from todolist import routes
 
+db_user = os.environ["CLOUD_SQL_USERNAME"]
+db_password = os.environ["CLOUD_SQL_PASSWORD"]
+db_name = os.environ["CLOUD_SQL_DATABASE_NAME"]
+db_connection_name = os.environ["CLOUD_SQL_CONNECTION_NAME"]
+
+if os.environ.get('GAE_ENV') == 'standard':
+    # If deployed, use the local socket interface for accessing Cloud SQL
+    unix_socket = '/cloudsql/{}'.format(db_connection_name)
+    cnx = pymysql.connect(user=db_user, password=db_password,
+                            unix_socket=unix_socket, db=db_name)
+else:
+    # If running locally, use the TCP connections instead
+    # Set up Cloud SQL Proxy (cloud.google.com/sql/docs/mysql/sql-proxy)
+    # so that your application can use 127.0.0.1:3306 to connect to your
+    # Cloud SQL instance
+    host = '127.0.0.1'
+    cnx = pymysql.connect(user=db_user, password=db_password,
+                            host=host, db=db_name)
+
+with cnx.cursor() as cursor:
+    cursor.execute('YOUR QUERY GOES HERE;')
+    result = cursor.fetchall()
+    current_msg = result[0][0]
+cnx.close()
+
+# return str(current_msg)
