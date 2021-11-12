d5541fde404793a00533dfd3a6a8c98b7b3d158e
Kevin Barker <kwkbarker@gmail.com>
2021-11-12 18:34:21 -0500

    fixed cache bug with js

--- todolist/static/apps-profile.js
+++ todolist/static/apps-profile.js
@@ -5,6 +5,6 @@ document.addEventListener('DOMContentLoaded', () => {
 function reload_profile_pic() {
     var rando = Math.floor(Math.random() * 200);
     profile_pic = document.getElementById('profile_pic');
-    profile_pic.src = profile_pic.src + rando
+    profile_pic.src = profile_pic.src + "?" + rando;
     console.log(profile_pic.src)    
 }
